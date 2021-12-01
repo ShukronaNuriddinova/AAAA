163a8b69e785a55e2a3974b2b70a21bc4ce19fd1
ShukronaNuriddinova <shfnuriddinova@edu.hse.ru>
2021-12-01 20:25:56 +0300

    Was added a script HW_1.1.sql
    Был добавлен скрипт, которые импортирует данные с файла  csv в БД

---
+++ HW_1.1.sql
@@ -0,0 +1,48 @@
+
+DROP TABLE IF EXISTS public.bond_quotes;
+
+CREATE TABLE public.bond_quotes
+(  Currency char(3) NOT NULL,
+   Date_trading date NOT NULL,
+   Exch_me varchar(70),
+   Bid_price real,
+   Ask_price real,
+   Opening_price numeric,
+   Indicative_price numeric,
+   Indicative_price_type varchar(6),
+   Turnover numeric, 
+   Number_of_trades smallint,
+   Coupon numeric,
+   YTM_ind numeric,
+   Coupon_accum numeric, 
+   Option_date date, 
+   Duration_option numeric,
+   Trade_regime varchar(1000),
+   ISIN varchar(15),
+   G_spread numeric,
+   State_bank boolean,
+   Issuer varchar(70),
+   Days_to_maturity smallint,
+   Total_days_to_maturity smallint,
+   Days_to_call smallint,
+   Callable boolean,
+   G_spread_interpolated numeric,
+   RF_interpolated numeric
+)
+WITH (
+    OIDS=FALSE
+)
+TABLESPACE pg_default;
+
+ALTER TABLE public.bond_quotes
+        OWNER to postgres;
+
+-- Формат даты для Маков
+
+ALTER DATABASE postgres SET datestyle TO "ISO, DMY";         
+
+-- Загрузить данные
+
+\copy public.bond_quotes FROM 'C:/Users/Public/bond_quotes.csv'  DELIMITER ';' CSV HEADER; 
+
+
